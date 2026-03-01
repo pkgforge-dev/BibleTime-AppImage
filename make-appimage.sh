259954af8aed2fee343e5e8880e4bcbe2b21395b
Link4Electronics <link4electronics@gmail.com>
2026-03-01 00:08:59 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,12 +8,13 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/scalable/apps/info.bibletime.BibleTime.svg
+export DESKTOP=/usr/share/applications/info.bibletime.BibleTime.desktop
+export STARTUPWMCLASS=
 export ALWAYS_SOFTWARE=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/bin/bibletime
 
 # Additional changes can be done in between here
 
