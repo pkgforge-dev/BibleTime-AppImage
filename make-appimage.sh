0fcde450e76eb4c43ae023002ea24a2fe6a72d4b
Link4Electronics <link4electronics@gmail.com>
2026-08-25 10:01:58 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -21,7 +21,3 @@ quick-sharun /usr/bin/bibletime
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
-
-# Test the app for 12 seconds, if the test fails due to the app
-# having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
